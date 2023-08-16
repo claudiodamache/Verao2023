8832a5275765ea445a258c9c4f9eccfa88a9775d
claudiodamache <136277423+claudiodamache@users.noreply.github.com>
2023-08-16 12:55:20 +0100

    Update script_main.py
    updated version 1.0

--- script_main.py
+++ script_main.py
@@ -6,11 +6,13 @@ import argparse
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description="Read JSON file")
-    parser.add_argument("-j", "--json", type=str)
-    parser.add_argument("-f", "--files", type=str)
-    parser.add_argument("-v", "--variables", type=list)
+    parser.add_argument("-jp", "--json_plot", type=str, default= "/lstore/cms/cbeiraod/Verao2023/plot2016.json")
+    parser.add_argument("-jv", "--json_var", type=str, default= "/lstore/cms/cbeiraod/Verao2023/variablesAN_DataMC.json")
+    parser.add_argument("-f", "--files", type=str, default= "/lstore/cms/cbeiraod/Verao2023/2016/")
+    parser.add_argument("-v", "--variables", type=list) #json file with variables
+    parser.add_argument("-unblind", "--data", type=bool, default=False )
     args = parser.parse_args()
     
-    graf = Plots(args.json, args.files, args.variables)
+    graf = Plots(args.json_plot, args.json_var, args.files, args.variables, args.data)
 
     graf.data_processing()
